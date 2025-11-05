0320fe51e56f9449656e76a8efd0d0704c72fcb5
root <carlgaspar@protonmail.com>
2025-11-05 01:03:38 +0000

    Updated Collections

--- tina/config.ts
+++ tina/config.ts
@@ -194,6 +194,73 @@ export default defineConfig({
           },
         ],
       },
+      {
+        format: "md",
+        label: "Now",
+        name: "now",
+        path: "content/now",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        format: "md",
+        label: "carldss.com",
+        name: "carldsscom",
+        path: "content/carldsscom",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        ui: {
+          filename: {
+            // if disabled, the editor can not edit the filename
+            readonly: false,
+            // Example of using a custom slugify function
+            slugify: values => {
+              // Values is an object containing all the values of the form. In this case it is {title?: string, topic?: string}
+              return `${values?.title ? values.title.toLowerCase().replace(/ /g, '-') : 'no-title'}`
+            },
+          },
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "title",
+          },
+          ...archetypeFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "About",
+        name: "about",
+        path: "content/about",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
     ],
   },
   search: {
